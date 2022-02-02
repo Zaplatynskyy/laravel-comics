846d93d12a149a3e8bd9c547fa6ddee93f90b964
Zaplatynskyy <zaplatynskyy@gmail.com>
2022-02-02 17:00:18 +0100

    header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,24 @@
 <header>
-    
+    <div class="container">
+        <!-- img logo header -->
+        <a href="/">
+            <img src="{{asset('img/dc-logo.png')}}" alt="logo dc comics">
+        </a>
+
+        <!-- link di collegamento header -->
+        <nav class="header_nav">
+            <ul>
+                <li><a href="/">Characters</a></li>
+                <li><a href="/" class="selected">Comics</a></li>
+                <li><a href="/">Movies</a></li>
+                <li><a href="/">Tv</a></li>
+                <li><a href="/">Games</a></li>
+                <li><a href="/">Collectibles</a></li>
+                <li><a href="/">Videos</a></li>
+                <li><a href="/">Fans</a></li>
+                <li><a href="/">News</a></li>
+                <li><a href="/">Shop</a></li>
+            </ul>
+        </nav>
+    </div>
 </header>
