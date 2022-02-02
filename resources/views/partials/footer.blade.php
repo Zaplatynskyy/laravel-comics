b29b65575b09fc8e243b62e259cd3092dc3273f4
Zaplatynskyy <zaplatynskyy@gmail.com>
2022-02-02 17:33:31 +0100

    footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,125 @@
 <footer>
-    
+    <div class="top_footer">
+        <div class="container">
+            <!-- lista link collegamento del top footer -->
+            <ul>
+
+                <li>
+                    <a href="#">
+                        <img src="{{asset('img/buy-comics-digital-comics.png')}}" alt="">
+                        <div class="text">digital comics</div>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="#">
+                        <img src="{{asset('img/buy-comics-merchandise.png')}}" alt="">
+                        <div class="text">dc merchandise</div>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="#">
+                        <img src="{{asset('img/buy-comics-subscriptions.png')}}" alt="">
+                        <div class="text">subscription</div>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="#">
+                        <img src="{{asset('img/buy-comics-shop-locator.png')}}" alt="">
+                        <div class="text">comic shop locator</div>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="#">
+                        <img src="{{asset('img/buy-dc-power-visa.svg')}}" alt="">
+                        <div class="text">dc power visa</div>
+                    </a>
+                </li>
+
+            </ul>
+        </div>
+    </div>
+
+    <div class="main_footer">
+        <div class="container">
+            <!-- link utili footer centrale -->
+            <nav class="footer_nav">
+                <!-- prima colonna link -->
+                <div class="ul_col one">
+                    <ul id="dc_comics">
+                        <li><a href="#">Dc Comics</a></li>
+                        <li><a href="#">Characters</a></li>
+                        <li><a href="#">Comics</a></li>
+                        <li><a href="#">Movies</a></li>
+                        <li><a href="#">TV</a></li>
+                        <li><a href="#">Games</a></li>
+                        <li><a href="#">Videos</a></li>
+                        <li><a href="#">News</a></li>
+                    </ul>
+
+                    <ul id="shop">
+                        <li><a href="#">Shop</a></li>
+                        <li><a href="#">Shop DC</a></li>
+                        <li><a href="#">Shop DC Collectibles</a></li>
+                    </ul>
+                </div>
+
+                <!-- seconda colonna link -->
+                <div class="ul_col two">
+                    <ul id="dc">
+                        <li><a href="#">Dc</a></li>
+                        <li><a href="#">Terme Of Use</a></li>
+                        <li><a href="#">Privacy policy (New)</a></li>
+                        <li><a href="#">Ad Choices</a></li>
+                        <li><a href="#">Advertising</a></li>
+                        <li><a href="#">Jobs</a></li>
+                        <li><a href="#">Subscriptions</a></li>
+                        <li><a href="#">Talent Workshops</a></li>
+                        <li><a href="#">CPSC Certificates</a></li>
+                        <li><a href="#">Rating</a></li>
+                        <li><a href="#">Shop Help</a></li>
+                        <li><a href="#">Contact Us</a></li>
+                    </ul>
+                </div>
+
+                <!-- terza colonna link -->
+                <div class="ul_col three">
+                    <ul id="sites">
+                        <li><a href="#">Sites</a></li>
+                        <li><a href="#">DC</a></li>
+                        <li><a href="#">MAD Magazine</a></li>
+                        <li><a href="#">DC Kids</a></li>
+                        <li><a href="#">DC Universe</a></li>
+                        <li><a href="#">DC Power Visa</a></li>
+                    </ul>
+                </div>
+            </nav>
+
+            <!-- logo footer centrale -->
+            <div class="footer_logo">
+                <img src="{{asset('img/dc-logo-bg.png')}}" alt="">
+            </div>
+        </div>
+    </div>
+
+    <div class="bottom_footer">
+        <div class="container">
+            <!-- bottone login del footer bottom -->
+            <button class="btn">Sign-up now!</button>
+
+            <!-- link social di collegamento del footer bottom -->
+            <ul class="social_nav">
+                <li><a href="#">Follow us</a></li>
+                <li><a href="#"><img src="{{asset('img/footer-facebook.png')}}" alt=""></a></li>
+                <li><a href="#"><img src="{{asset('img/footer-twitter.png')}}" alt=""></a></li>
+                <li><a href="#"><img src="{{asset('img/footer-youtube.png')}}" alt=""></a></li>
+                <li><a href="#"><img src="{{asset('img/footer-periscope.png')}}" alt=""></a></li>
+                <li><a href="#"><img src="{{asset('img/footer-pinterest.png')}}" alt=""></a></li>
+            </ul>
+
+        </div>
+    </div>
 </footer>
